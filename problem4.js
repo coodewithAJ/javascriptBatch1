6c1b54e494568c1c3f45b40d2617ab998309d5fc
coodewithAJ <75101047+coodewithAJ@users.noreply.github.com>
2022-01-29 18:05:29 +0530

    Add files via upload

---
+++ problem4.js
@@ -0,0 +1,12 @@
+let number = -8;
+      if (number>0){
+          console.log("the number is positive");
+      }
+      else if (number==0){
+        console.log("the number is 0");
+
+      }
+      else if (number<0){
+        console.log("the number is negative");
+
+      }
